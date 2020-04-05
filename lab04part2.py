baac02bc554b067b5cdbfde1a97f666ae5f88e78
Piotr Grajeta <piotr@fedora.localdomain>
2020-04-05 18:30:10 +0200

    started second part of the exercise

---
+++ lab04part2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import sys
+import operator
+
+hist = []
+letters = {}
+for i in range(ord('A'), ord('Z')+1):
+    letters[chr(i)] = 0
+    
+if len(sys.argv) == 1:
+    sys.exit("No file given!")
+    
+text = sys.argv[1]
+
+number_of_chars = 1
+try:
+    with open(text) as file:
+        for line in file:
+            for char in line:
+                if char in letters:
+                    number_of_chars += 1
+                    letters[char] += 1
+    with open(text) as file:
+        all_lines = file.readlines()
+        first_line = all_lines[0]
+        for char in first_line.strip():
+            hist.append(char)
+
+                
+except FileNotFoundError:
+    print('Error')
+letters = dict(sorted(letters.items(), key=operator.itemgetter(1), reverse=True))
+for k,v in (letters.items()):
+    print('{0:<8}{1:<8}{2:.2f}%'.format(k,v,v/number_of_chars))
+
+print(f'Number of letters in text: {number_of_chars}')
+print(hist)
+first_letter = list(letters.keys())[0]
+print((ord(first_letter) - ord(hist[0])) % len(hist))
+
