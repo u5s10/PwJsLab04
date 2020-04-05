63d5e30cc1d7bc70815725a620cc746f3e9631e3
Piotr Grajeta <piotr@fedora.localdomain>
2020-04-05 14:30:51 +0200

    first part of the exercise

---
+++ lab04part1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import sys
+import operator
+
+letters = {}
+for i in range(ord('A'), ord('Z')+1):
+    letters[chr(i)] = 0
+for i in range(ord('a'), ord('z')+1):
+    letters[chr(i)] = 0
+
+    
+if len(sys.argv) == 1:
+    sys.exit("No file given!")
+
+text = sys.argv[1]
+
+number_of_chars = 0
+try:
+    with open(text) as file:
+        for line in file:
+            for char in line:
+                if char in letters:
+                    number_of_chars += 1
+                    letters[char] += 1
+            
+except FileNotFoundError:
+    print('Error')
+
+for k,v in sorted(letters.items(), key=operator.itemgetter(1), reverse=True):
+    print('{0:<8}{1:<8}{2:.2f}%'.format(k,v,v/number_of_chars))
+
+print(f'Number of letters in text: {number_of_chars}')
